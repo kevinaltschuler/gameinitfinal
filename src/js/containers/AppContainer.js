c72ffde24e2cd5551b8e3de05192bde2c0a4bfb6
kevin altschuler <kevin@kevins-MacBook-Pro.local>
2018-11-28 13:28:59 -0500

    everyhting

--- src/js/containers/AppContainer.js
+++ src/js/containers/AppContainer.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import mapAPIkey from '../../CREDS.js';
 
-const AppContainer = (props) => (
-  <div>
-    <h1>Hello, React!</h1>
-  </div>
-);
+class AppContainer extends Component {
+	static defaultProps = {
+	    center: {
+	      lat: 59.95,
+	      lng: 30.33
+	    },
+	    zoom: 11
+	};
+ 
+  	render() {
+	  	return {
+		  	<div style={{ height: '100vh', width: '100%' }}>
+			    <GoogleMapReact
+			      bootstrapURLKeys={{ key: mapAPIkey }}
+			      defaultCenter={this.props.center}
+			      defaultZoom={this.props.zoom}
+			    >
+			    </GoogleMapReact>
+			 </div>
+		};
+	}
+}
 
 export default AppContainer;
